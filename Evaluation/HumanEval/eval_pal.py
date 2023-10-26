73efd7ae0e689cc90f63c31162280231fd6f3602
zhuqh <zhuqh@deepseek.com>
2023-10-26 17:57:25 +0800

    init commit

--- Evaluation/HumanEval/eval_pal.py
+++ Evaluation/HumanEval/eval_pal.py
@@ -28,13 +28,15 @@ if __name__ == '__main__':
     logdir = args.logdir
     language = args.language
 
+    if logdir == "":
+        logdir = "tmp/"
     tokenizer = dict(
         cls=AutoTokenizer,
         model_path=logdir,)
 
     dataroot = args.dataroot
 
-    evaluator = evaltor(data_root=dataroot, max_seq_len=4096, tokenizer_cfg=tokenizer, log_dir="tmp/", n_sample=1, batch_size=1, language=language, max_gen_len=500)
+    evaluator = evaltor(data_root=dataroot, max_seq_len=4096, tokenizer_cfg=tokenizer, log_dir=logdir, n_sample=1, batch_size=1, language=language, max_gen_len=500)
     model = AutoModelForCausalLM.from_pretrained(logdir, device_map=accelerator.device, trust_remote_code=True, torch_dtype=torch.bfloat16)
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     evaluator.eval_model(model, accelerator)
